360389ea32452c6cec28147d6771916c43210865
adr-69 <bindgarenacod@gmail.com>
2025-08-10 15:09:01 +0800

    update

--- appjs/app.js
+++ appjs/app.js
@@ -1,6 +1,4 @@
 function back() { window.location.href = "../akira.html"; }
 function non() { alert("Under Development") }
-function OralCom() { window.location.href = "../pageshtml/oralcom.htm"; }
-function EmpTech() { window.location.href = "../pageshtml/emptech.htm"; }
 
 
